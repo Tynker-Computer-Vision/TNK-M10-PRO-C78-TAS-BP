138da777ccb880cd94a0450a3331fa48c520677b
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-06-30 08:36:17 +0530

    Delete TAS-BP directory

--- TAS-BP/TA1/helper.py
+++
@@ -1,7 +0,0 @@
-import math 
-
-def getSensorX(x, angle, sensorAngle, margin):
-    return int(x -(margin * math.cos(math.radians(angle+sensorAngle))))
-
-def getSensorY(y, angle, sensorAngle, margin):
-    return int(y +(margin * math.sin(math.radians(angle+sensorAngle))))
